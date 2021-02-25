c0148bb486d5e6b531b6bcdd191281ed3e1f5f8f
Michael A. Cook <mcook0775@gmail.com>
2021-02-24 21:40:47 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# File Collector
+
+A simple program to programmatically get and output an array of files. This is useful when you need a list of files but manually compiling a list would be too labour intensive.
+
+## Installation
+
+Run `npm install -g file-collector`
+
+## Usage
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+const fs = require("fs")
+const args = process.argv.slice(1)
+let filter = null
+let exclude = null
+let extension = null
+
+if (args.length > 1) {
+  let options = args.slice(1)
+  options = options.map((str) => str.split("="))
+  options.forEach((pair) => {
+    const [key, value] = pair
+    switch (key.toLowerCase()) {
+      case "filter":
+        filter = value
+        break
+      case "exclude":
+        if (value.includes(",")) {
+          exclude = value.split(",")
+        } else {
+          exclude = value
+        }
+        break
+      case "extension":
+        extension = value
+        break
+    }
+  })
+}
+
+fs.readdir(
+  "./",
+  {
+    withFileTypes: true,
+  },
+  (err, files) => {
+    if (err) {
+      console.log(
+        "Something went wrong and the program couldn't complete the task."
+      )
+      process.exit()
+    }
+    let dir = files.map((dirent) => dirent.name)
+    if (extension) {
+      dir = dir.filter((file) => {
+        const ext = file.slice(file.indexOf("."))
+        return ext === extension
+      })
+    }
+    if (exclude) {
+      if (Array.isArray(exclude)) {
+        for (let item of exclude) {
+          dir = dir.filter((file) => !file.includes(item))
+        }
+      } else {
+        dir = dir.filter((file) => !file.includes(exclude))
+      }
+    }
+    if (filter) {
+      dir = dir.filter((file) => file.includes(filter))
+    }
+    const stream = fs.createWriteStream("files-array.js")
+    stream.write("// import me elsewhere or copy the array :) \n\n")
+    stream.write(`export default\n[\n`)
+    dir.forEach((file) => {
+      stream.write(`  "${file}",\n`)
+    })
+    stream.write("]")
+    stream.end()
+    console.log("Your array is in files-array.js in the current working tree")
+  }
+)
